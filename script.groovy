32540098cb1f7b81f3ace2aea3d1010c7a731488
Lord-lami <48473978+Lord-lami@users.noreply.github.com>
2025-02-21 11:16:33 +0100

    making the uniform script.groovy for all branches

--- script.groovy
+++ script.groovy
@@ -3,14 +3,24 @@ def buildApp() {
 }
 
 def testApp() {
+    echo "On the pipeline of " + env.BRANCH_NAME
     if (params.executeTest) {
         echo "Testing the application..."
     }
 }
 
 def deployApp() {
+    selectedEnv = input(
+        message: "What environment do you want to deploy to?",
+        parameters: [
+            choice(name: 'Env1', choices: ['dev', 'test', 'prod'], description: 'First Environment to deploy to'),
+            choice(name: 'Env2', choices: ['dev', 'test', 'prod'], description: 'Second Environment to deploy to')
+        ]
+    )
     echo "Deploying the application..."
     echo "Deploying version ${params.Version}"
+    echo "Deploying to " + selectedEnv['Env1']
+    echo "Deploying to " + selectedEnv['Env2']
 }
 
 return this
