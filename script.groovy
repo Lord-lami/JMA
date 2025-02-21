25ce27c4851cf2c6e1d99156af938476023ad5b1
Lord-lami <48473978+Lord-lami@users.noreply.github.com>
2025-02-21 11:15:08 +0100

    making script.groovy uniform across branches

--- script.groovy
+++ script.groovy
@@ -1,19 +1,26 @@
-def buildJar() {
-    echo "building the application..."
-    sh 'mvn package'
-} 
+def buildApp() {
+    echo('Building the application...')
+}
 
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+def testApp() {
+    echo "On the pipeline of " + env.BRANCH_NAME
+    if (params.executeTest) {
+        echo "Testing the application..."
     }
-} 
+}
 
 def deployApp() {
-    echo 'deploying the application...'
-} 
+    selectedEnv = input(
+        message: "What environment do you want to deploy to?",
+        parameters: [
+            choice(name: 'Env1', choices: ['dev', 'test', 'prod'], description: 'First Environment to deploy to'),
+            choice(name: 'Env2', choices: ['dev', 'test', 'prod'], description: 'Second Environment to deploy to')
+        ]
+    )
+    echo "Deploying the application..."
+    echo "Deploying version ${params.Version}"
+    echo "Deploying to " + selectedEnv['Env1']
+    echo "Deploying to " + selectedEnv['Env2']
+}
 
 return this
